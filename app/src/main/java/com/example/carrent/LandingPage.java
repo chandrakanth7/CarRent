65360cbc48ba3b7e50cf3ca0c67696e6a16ac24f
chandrakanth7 <42292161+chandrakanth7@users.noreply.github.com>
2023-03-03 13:45:23 -0600

    Added implementation to LandingPage, when user starts the app he will be visiting LandingPage which acts as home page for our app.

--- app/src/main/java/com/example/carrent/LandingPage.java
+++ app/src/main/java/com/example/carrent/LandingPage.java
@@ -8,12 +8,22 @@ import androidx.cardview.widget.CardView;
 
 public class LandingPage extends AppCompatActivity {
 
-    CardView login, register, about;
+    CardView login,register,about;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_page);
+
+        login = findViewById(R.id.cardLogin);
+        register = findViewById(R.id.cardRegister);
+        about = findViewById(R.id.cardAboutus);
+
+        login.setOnClickListener(v -> startActivity(new Intent(this, LogIn.class)));
+
+        register.setOnClickListener(v -> startActivity(new Intent(this, Signup.class)));
+
+        about.setOnClickListener(v -> startActivity(new Intent(this, About.class)));
+
     }
 }
