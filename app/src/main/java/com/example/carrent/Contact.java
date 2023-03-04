bfd24fe872773c33e3046c8816cc1d3752469ec1
DhruvaTej345 <dhruvatej3094@gmail.com>
2023-03-03 18:29:16 -0600

    Modified contact layout, resolved errors regarding speakable text and added some material design to buttons and other components. Made changes to Contact.java added code for submit button, functionality yet to be added

--- app/src/main/java/com/example/carrent/Contact.java
+++ app/src/main/java/com/example/carrent/Contact.java
@@ -1,14 +1,38 @@
 package com.example.carrent;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Contact extends AppCompatActivity {
+
+    private EditText nameEditText, emailEditText, messageEditText;
+    private Button sendButton;
+
+    private String recipientEmail = "example@example.com";
+    private String senderEmail = "sender@example.com";
+    private String senderPassword = "password";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contact);
+
+        nameEditText = (EditText) findViewById(R.id.name);
+        emailEditText = (EditText) findViewById(R.id.email);
+        messageEditText = (EditText) findViewById(R.id.message);
+        sendButton = (Button) findViewById(R.id.submit);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //sendFeedback();
+            }
+        });
+
     }
 }
 
