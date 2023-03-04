ddaa511899e49036ff8925d998d828cd908830df
DhruvaTej345 <dhruvatej3094@gmail.com>
2023-03-03 18:59:08 -0600

    Modified contact layout, added a text view with a description and added functionality to check weather the fields are empty when submitted and send appropriate response in the text view.

--- app/src/main/java/com/example/carrent/Contact.java
+++ app/src/main/java/com/example/carrent/Contact.java
@@ -1,15 +1,18 @@
 package com.example.carrent;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Contact extends AppCompatActivity {
 
     private EditText nameEditText, emailEditText, messageEditText;
+    private TextView confomationText;
     private Button sendButton;
 
     private String recipientEmail = "example@example.com";
@@ -24,12 +27,19 @@ public class Contact extends AppCompatActivity {
         nameEditText = (EditText) findViewById(R.id.name);
         emailEditText = (EditText) findViewById(R.id.email);
         messageEditText = (EditText) findViewById(R.id.message);
+        confomationText = findViewById(R.id.conform);
         sendButton = (Button) findViewById(R.id.submit);
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //sendFeedback();
+                if(!TextUtils.isEmpty(nameEditText.getText().toString()) && !TextUtils.isEmpty(emailEditText.getText().toString()) && !TextUtils.isEmpty(messageEditText.getText().toString())) {
+                    //sendFeedback();
+                    confomationText.setText("Thank for your Feedback");
+                }
+                else{
+                    confomationText.setText("Please enter all the details");
+                }
             }
         });
 
