2a9a9456a2da968f977c929672b03300782d544b
DhruvaTej345 <dhruvatej3094@gmail.com>
2023-03-03 19:49:58 -0600

    History page added new fonts and added a empty history message in tne history card view added colours for the cards and background and in history activity added function to delete the history deletion function is yet to be implemented. In addition added some icons that are related to app.

--- app/src/main/java/com/example/carrent/History.java
+++ app/src/main/java/com/example/carrent/History.java
@@ -2,9 +2,12 @@ package com.example.carrent;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class History extends AppCompatActivity {
 
@@ -12,5 +15,17 @@ public class History extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.history);
+
+        TextView data = findViewById(R.id.data);
+        CardView deleteButton = findViewById(R.id.delete);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!(data.getText().toString() == "Empty History")) {
+                    //deleteAllHistory();
+                    data.setText("Empty History");
+                }
+            }
+        });
     }
 }
