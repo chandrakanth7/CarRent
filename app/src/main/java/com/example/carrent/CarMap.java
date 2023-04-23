eec1bcbc4670987c27c7b11cf0429a584b8df4cc
chandrakanth7 <42292161+chandrakanth7@users.noreply.github.com>
2023-04-23 16:32:17 -0500

    Implemented logic for displaying all the Renter's locations on the map.

--- app/src/main/java/com/example/carrent/CarMap.java
+++ app/src/main/java/com/example/carrent/CarMap.java
@@ -1,14 +1,141 @@
 package com.example.carrent;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-public class CarMap extends AppCompatActivity
+public class CarMap extends AppCompatActivity implements OnMapReadyCallback, LocationListener
 {
+    private GoogleMap mMap;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    private int REQUEST_CODE = 11;
+    SupportMapFragment mapFragment;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    public static final String TAG = "TAG";
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.carmap);
+
+      fAuth=FirebaseAuth.getInstance();
+      fStore= FirebaseFirestore.getInstance();
+
+      mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+      if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+          mapFragment.getMapAsync(this);
+      } else {
+          ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+      }
+
+      CollectionReference docRef = FirebaseFirestore.getInstance().collection("carRentals");
+      docRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+          @Override
+          public void onComplete(@NonNull Task<QuerySnapshot> task) {
+              if(task.isSuccessful()){
+                  for(QueryDocumentSnapshot document:task.getResult()){
+                      GeoPoint location = document.getGeoPoint("location");
+                      double latitude = location.getLatitude();
+                      double longitude = location.getLongitude();
+                      LatLng latLng = new LatLng(latitude, longitude);
+                      mMap.addMarker(new MarkerOptions().position(latLng)
+                          .title("Renter"));
+                      mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                  }
+              }
+          }
+      });
   }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        mLastLocation = location;
+        LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You are here");
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+        mMap.addMarker(markerOptions).showInfoWindow();
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Check if location permission is granted
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+
+            // Get the user's current location
+            FusedLocationProviderClient locationClient =
+                    LocationServices.getFusedLocationProviderClient(this);
+            locationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                // Add a marker at the user's current location
+                                mLastLocation = location;
+                                LatLng currentLatLng = new LatLng(location.getLatitude(),
+                                        location.getLongitude());
+                                mMap.addMarker(new MarkerOptions().position(currentLatLng)
+                                        .title("You're here"));
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15));
+                            }
+                        }
+                    });
+        } else {
+            // Request location permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+    }
+
+    // Handle the result of the location permission request
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                onMapReady(mMap);
+            }
+        }
+    }
 }
